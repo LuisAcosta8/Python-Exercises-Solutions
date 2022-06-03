d7936eae792d240ca4f6947af17b4393ac6f814c
Luis Acosta <acostabluis8@gmail.com>
2022-06-03 16:32:46 -0500

    Basic Excersices.py

---
+++ Basic Excersices.py
@@ -0,0 +1,18 @@
+# Basic
+#  1) Write a program that prompts the user to type their name and greets them by calling their name.
+#  2) Write a program that receives as input the radius of a circle and outputs its perimeter and area.
+#  3) Write a program that calculates the average of 4 marks entered by the user
+#  4) Write a program that converts from centimeters to inches. An inch is 2.54 centimeters
+#  5) Write a program that prompts the user for a three-digit integer and returns the number with the digits in reverse order
+#  6) Write a program that receives as input the lengths of the two legs a and b of a right triangle, and outputs the length of the hypotenuse c of the triangle, given by the Pythagorean theorem: c^2=a^2+b^2
+#  7) Write a program that asks the user for the current clock time t and an integer number of hours h, indicating what time the clock will show in h hours
+#  8) Write a program that returns the decimal part of a real number entered by the user
+#  9) A student wants to know what mark he needs in the third contest to pass a bouquet.
+"""    The branch average is calculated using the following formula.
+
+    NC=(C1+C2+C3)/3
+    NF=NC⋅0.7+NL⋅0.3
+    Where NC is the average of the exam, NL the average of the laboratory and NF the final grade.
+
+    Write a program that asks the user for the marks of the first two quizzes and the laboratory mark, and shows the mark that the student needs to pass the course with a final mark of 60 
+    """
